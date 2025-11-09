d5391671c5634c236ae3f2c175f0f9969cd894e7
Jen-Chieh Shen <jcs090218@gmail.com>
2025-11-10 00:37:54 +0800

    feat: add backup script

--- backup.sh
+++ backup.sh
@@ -3,6 +3,6 @@
 ERR_STR="Unexpected error"
 
 if grep -q "$ERR_STR" "$1"; then
-    cp -r ./backup/$1 $1
-    echo "Recover the file `$1` from `backup/$1`."
+    cp -r $1 ./backup/$1
+    echo "Backup the file `$1` to `backup/$1`."
 fi
